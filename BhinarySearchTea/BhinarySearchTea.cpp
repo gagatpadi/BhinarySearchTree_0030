3db5f92a48cb4df3d38670bfd2588e13736ac9fd
gagatpadi <padiyogaradhana@gmail.com>
2024-06-13 18:40:05 +0700

    lanjutan void insert (2)

--- BhinarySearchTea/BhinarySearchTea.cpp
+++ BhinarySearchTea/BhinarySearchTea.cpp
@@ -38,5 +38,17 @@ public:
 		Node* parent = nullptr;
 		Node* currentNode = nullptr;
 		search(element, parent, currentNode); // locate the node which be the parent of the node to be insert
+
+		if (parent == nullptr) // if the parent is null (tree is emprty)
+		{
+			ROOT = newNode; // // mark the new node as root
+			return; // exit
+		}
+
+		if (element < parent->info) // if the value in the data field pf the new node is less than that of the parent
+		{
+			parent->leftchild = newNode; // Make the left child of the parent point to the new node		
+		}
+		else if (element > parent->info) // if the value in the data field of the new data is greater than that of the parent
 	}
 };
