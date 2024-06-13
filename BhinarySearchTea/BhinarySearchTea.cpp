7dd8acdd709b7f5affe9879291d02b626dc2662a
gagatpadi <padiyogaradhana@gmail.com>
2024-06-13 18:33:00 +0700

    void insert

--- BhinarySearchTea/BhinarySearchTea.cpp
+++ BhinarySearchTea/BhinarySearchTea.cpp
@@ -28,4 +28,15 @@ public:
 		ROOT = nullptr; // Initializing Root to null
 	}
 
-	
+	void insert(string element) // Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the new node
+		newNode->info = element; // assign value to the data field of the new data
+		newNode->leftchild = nullptr; // make the left child of the new node point tol Null
+		newNode->rightchild = nullptr; // make the right child of the new data point to Null
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+		search(element, parent, currentNode); // locate the node which be the parent of the node to be insert
+	}
+};
