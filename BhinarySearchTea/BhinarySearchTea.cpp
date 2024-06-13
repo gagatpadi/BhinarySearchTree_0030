2526a7dcbb871fc620b15a77805c9511b98722c6
gagatpadi <padiyogaradhana@gmail.com>
2024-06-13 20:44:29 +0700

    switch case 1

--- BhinarySearchTea/BhinarySearchTea.cpp
+++ BhinarySearchTea/BhinarySearchTea.cpp
@@ -127,4 +127,14 @@ int main() {
 		cin >> ch;
 		cout << endl;
 
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			x.insert(word);
+			break;
+		}
 		
